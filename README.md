c74aa8901e6647093eb8c2a5889e500d06b5d84f
Michael N. Brown <michael@spreedly.com>
2017-09-22 10:40:40 -0400

    Blcok and blockchain implemented

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blockchain in Golang
+Source: [Building Blockchain](https://jeiwan.cc/posts/building-blockchain-in-go-part-1/)
+
+## Installation
+1. clone repo in your `$GOPATH/src` directory.
+2. `go build`
+3. `./blockchain-go`
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func main() {
+	bc := NewBlockchain()
+
+	bc.AddBlock("Send 1BTC to Josh")
+	bc.AddBlock("Send 3BTC to Erich")
+
+	for _, block := range bc.blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+}
